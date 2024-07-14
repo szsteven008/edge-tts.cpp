c207cdb55d8145ed7e14bf15ec14ce60c8044fd5
szsteven008 <97944818+szsteven008@users.noreply.github.com>
2024-07-14 16:39:48 +0800

    Add files via upload
    tts_request modified

--- include/tts.h
+++ include/tts.h
@@ -8,5 +8,8 @@ using namespace std;
 
 int voices_list(vector<tuple<string, string>>& voices);
 int tts_open(const string& rate, const string pitch, const string& volume);
-int tts_request(const string& voice, const string& text, const string& outfile);
+int tts_request(const string& voice, 
+                const string& text, 
+                vector<tuple<int, int, string>>& meta, 
+                vector<string>& data);
 int tts_close();
